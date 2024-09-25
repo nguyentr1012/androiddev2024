6bd56d7f161617f6ca31cbe6149a34d9c01ca118
nguyentr1012 <nguyentt.22bi13343@usth.edu.vn>
2024-09-25 22:15:26 +0700

    practical 11
    upload prac11

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,14 +1,46 @@
 package vn.edu.usth.weather;
 
 import android.os.Bundle;
+import android.content.pm.PackageManager;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import com.google.android.material.tabs.TabLayout;
 import android.util.Log;
 
+import android.media.MediaPlayer;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 public class WeatherActivity extends AppCompatActivity {
-
+    //extract and play music part
+    private void extractAndPlayMusic() {
+        try {
+            InputStream inputStream = getResources().openRawResource(R.raw.wii_sound);
+            File musicFile = new File(getExternalFilesDir(null), "music.mp3");
+            OutputStream outputStream = new FileOutputStream(musicFile);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = inputStream.read(buffer)) > 0){
+                outputStream.write(buffer, 0, length);
+            }
+            outputStream.flush();
+            outputStream.close();
+            inputStream.close();
+            // Play
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource(musicFile.getPath());
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+//viewpager part
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +49,7 @@ public class WeatherActivity extends AppCompatActivity {
         ViewPager viewPager = findViewById(R.id.viewPager);
         HomeAdapter adapter = new HomeAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
         viewPager.setAdapter(adapter);
-
+        extractAndPlayMusic();
         TabLayout tabLayout = findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(viewPager);
 
@@ -27,6 +59,8 @@ public class WeatherActivity extends AppCompatActivity {
         tabLayout.getTabAt(2).setText("Toulouse,France");
     }
 
+
+    //log part
     @Override
     protected void onStart() {
         super.onStart();
@@ -56,4 +90,5 @@ public class WeatherActivity extends AppCompatActivity {
         super.onDestroy();
         Log.i("destroy", "onDestroy called");
     }
+
 }
